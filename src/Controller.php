8860d89fc6943ef83d963d1419b31dd2e27b5571
快乐浪子哥 <hulangfy@163.com>
2024-05-02 12:54:55 +0800

    update

---
+++ src/Controller.php
@@ -0,0 +1,255 @@
+<?php
+
+declare(strict_types=1);
+
+namespace hulang\apidoc;
+
+use think\facade\Request;
+use think\facade\View;
+
+class Controller
+{
+
+    protected $assets_path = '';
+    protected $view_path = '';
+    protected $doc = '';
+    protected $route_prefix = '';
+    protected $root = '';
+
+    protected $request; # Request 实例
+    protected $view; # 视图类实例
+
+    # 资源类型
+    protected $mimeType = [
+        'xml' => 'application/xml,text/xml,application/x-xml',
+        'json' => 'application/json,text/x-json,application/jsonrequest,text/json',
+        'js' => 'text/javascript,application/javascript,application/x-javascript',
+        'css' => 'text/css',
+        'rss' => 'application/rss+xml',
+        'yaml' => 'application/x-yaml,text/yaml',
+        'atom' => 'application/atom+xml',
+        'pdf' => 'application/pdf',
+        'text' => 'text/plain',
+        'png' => 'image/png',
+        'jpg' => 'image/jpg,image/jpeg,image/pjpeg',
+        'gif' => 'image/gif',
+        'csv' => 'text/csv',
+        'html' => 'text/html,application/xhtml+xml,*/*',
+    ];
+    public function __construct()
+    {
+        //有些程序配置了默认json问题
+        $this->assets_path = __DIR__ . '/assets/';
+        $this->doc = new Doc(config('apidoc'));
+        $this->route_prefix = $this->doc->route_prefix;
+        $config = [
+            'view_path' => __DIR__ . '/view/',
+            'default_filter' => '',
+        ];
+
+        View::config(['view_path' => __DIR__ . '/view/', 'view_suffix' => 'html']);
+
+        View::assign('web', $this->doc->__get());
+        View::assign('route_prefix', $this->route_prefix);
+
+        $this->assets_path = $this->doc->__get('static_path') ?: '/static/' . $this->route_prefix;
+        View::assign('assets', $this->assets_path);
+        $this->root = request()->root() ?: request()->domain();
+        if (
+            request()->session($this->route_prefix . '.is_login') !== $this->doc->__get('password')
+            && $this->doc->__get('password')
+            && request()->url() !== '/' . $this->route_prefix . '/login'
+            && stristr(request()->url(), '/assets') == false
+        ) {
+            session($this->route_prefix . '.request_url', Request::url(true));
+            header('location:/' . $this->route_prefix . '/login');
+            exit();
+        }
+
+        // 序言文档
+        View::assign('document', $this->doc->__get('document'));
+
+        // 分类
+        View::assign('versions', $this->doc->__get('api_type'));
+
+        // 左侧菜单
+        View::assign('menu', $this->doc->get_api_list(input('version', 0, 'intval')));
+    }
+
+    /**
+     * 解析资源
+     *
+     * @return mixed|array|bool
+     */
+    public function assets()
+    {
+        $assets_path = __DIR__ . '/assets/';
+        $path = str_replace($this->route_prefix . '/assets', '', request()->pathinfo());
+        $ext = request()->ext();
+        if ($ext) {
+            $type = 'text/html';
+            $content = file_get_contents($assets_path . $path);
+            if (array_key_exists($ext, $this->mimeType)) {
+                $type = $this->mimeType[$ext];
+            }
+            return response($content, 200, ['Content-Length' => strlen($content)])->contentType($type);
+        }
+    }
+    /**
+     * 显示函数
+     *
+     * @return mixed|array|bool
+     */
+    public function totrue($data)
+    {
+        return json(['code' => 200, 'msg' => 'ok', 'data' => $data]);
+    }
+    /**
+     * 显示模板
+     *
+     * @return mixed|array|bool
+     */
+    protected function template($name, $vars = [], $config = [])
+    {
+        $vars = array_merge(['root' => $this->root], $vars);
+        return View($name);
+    }
+    /**
+     * [index]模块
+     *
+     * @return mixed|array|bool
+     */
+    public function index()
+    {
+        return $this->template('index');
+    }
+    /**
+     * 模块
+     * @param string $name 模块名
+     *
+     * @return mixed|array|bool
+     */
+    public function module($name = '')
+    {
+        if (class_exists($name)) {
+            $reflection = new \ReflectionClass($name);
+            $doc_str = $reflection->getDocComment();
+            $doc = new Parser();
+            # 解析类
+            $class_doc = $doc->parse_class($doc_str);
+            View::assign('data', $class_doc);
+        }
+        return $this->template('module');
+    }
+    /**
+     * 方法
+     * @param string $name 方法名
+     *
+     * @return mixed|array|bool
+     */
+    public function action($name = '')
+    {
+        if (request()->isAjax()) {
+            list($class, $action) = explode('::', $name);
+            $data = $this->doc->get_api_detail($class, $action);
+            $data['is_header'] = $this->doc->__get('is_header');
+            # 全局header
+            $data['_header'] = $this->doc->__get('header');
+            # 全局参数
+            $data['_params'] = $this->doc->__get('params');
+            return $this->totrue($data);
+        } else {
+            return $this->template('action');
+        }
+    }
+    /**
+     * 文档
+     * @param string $name 文档名
+     *
+     * @return mixed|array|bool
+     */
+    public function document($name = 'explain')
+    {
+        if ($name == 'code') {
+            $data['list'] = config('apidoc')[$name];
+            View::assign('data', $data);
+        } else {
+            View::assign('data', $this->doc->__get('document')[$name]);
+        }
+        // dd($this->template('doc_' . $name));
+        return $this->template('doc_' . $name);
+    }
+    /**
+     * 格式化参数
+     *
+     * @return mixed|array|bool
+     */
+    // debug 格式化参数
+    public function format_params()
+    {
+        $header = $this->format(request()->param('header'));
+        // $header['Cookie'] = request()->param('cookie');
+        // $header['token'] = request()->param('token');
+
+        $url = request()->param('url');
+        $method = request()->param('method');
+
+        $data = $this->format(request()->param('params'));
+
+        if ($method == 'API') {
+            $arr = explode('::', $url);
+            return $this->totrue([$arr[0], $arr[1], $data]);
+        }
+
+        return $this->totrue(['params' => $data, 'header' => $header]);
+    }
+    /**
+     * 格式化
+     * 
+     * @param array $data 数据
+     * @return mixed|array|bool
+     */
+    private function format($data = [])
+    {
+        if (!$data || count($data) < 1) {
+            return [];
+        }
+        $result = [];
+        foreach ($data['name'] as $k => $v) {
+            $result[$v] = $data['value'][$k];
+        }
+        return $result;
+    }
+    /**
+     * 登陆
+     *
+     * @return mixed|array|bool
+     */
+    public function login()
+    {
+        if (request()->isPost()) {
+            if (input('post.password') != $this->doc->__get('password')) {
+                die('<script>alert("密码错误！");window.location.href="/' . $this->route_prefix . '";</script>');
+            } else {
+                session($this->route_prefix . '.is_login', input('post.password'));
+                return redirect(session($this->route_prefix . '.request_url') ?: '/' . $this->route_prefix);
+            }
+        } else {
+            if (session($this->route_prefix . '.is_login') == $this->doc->__get('password')) {
+                header('location:/' . $this->route_prefix);
+            } else {
+                return $this->template('login');
+            }
+        }
+    }
+    /**
+     * 注销登陆
+     *
+     * @return mixed|array|bool
+     */
+    public function outlogin()
+    {
+        session($this->route_prefix . '.is_login', null);
+        return redirect('/' . $this->route_prefix);
+    }
+}
---
+++ src/Doc.php
@@ -0,0 +1,211 @@
+<?php
+
+declare(strict_types=1);
+
+namespace hulang\apidoc;
+
+class Doc
+{
+    protected $config = [
+        'title' => 'APIDOC', # 文档title
+        'version' => '1.0.0', # 文档版本
+        'copyright' => 'Powered By x_mier', # 版权信息
+        'password' => '123123', # 访问密码，为空不需要密码
+        'document' => [
+            'explain' => [
+                'name' => '说明',
+                'list' => [],
+            ],
+            'code' => [
+                'name' => '返回码(复制codemsg)',
+                'list' => [],
+            ],
+        ],
+        'is_header' => 1,
+        // 全局请求header,一般存放token之类的
+        'header' => [],
+        // 全局请求参数
+        'params' => [
+            '__uid' => 2,
+        ],
+        // API分类
+        'api_type' => [],
+        // 过滤、不解析的方法名称
+        'filter_method' => [],
+        'static_path' => '/static/plug',
+        'return_format' => [
+            'status' => '200/300/301/302',
+            'message' => '提示信息',
+        ],
+    ];
+
+    protected $actionkey = [
+        'title' => '未定义方法标题',
+        'desc' => '未定义方法描述',
+        'author' => '无名氏',
+        'version' => '1.0',
+        'param' => [],
+        'return' => [],
+        'url' => '',
+        'method' => '',
+        'href' => '',
+    ];
+
+    protected $classkey = [
+        'title' => '未定义标题',
+        'desc' => '未定义描述',
+        'class' => '',
+        'action' => [],
+    ];
+
+    /**
+     * 架构方法 设置参数
+     *
+     * @access public
+     *
+     * @param  array $config 配置参数
+     */
+    public function __construct($config = [])
+    {
+        $this->config = array_merge($this->config, $config);
+    }
+
+    /**
+     * 使用 $this->name 获取配置
+     *
+     * @access public
+     *
+     * @param  string $name 配置名称
+     *
+     * @return mixed    配置值
+     */
+    public function __get($name = null)
+    {
+        if ($name) {
+            return $this->config[$name];
+        } else {
+            return $this->config;
+        }
+    }
+
+    /**
+     * 设置
+     *
+     * @access public
+     *
+     * @param  string $name  配置名称
+     * @param  string $value 配置值
+     *
+     * @return void
+     */
+    public function __set($name, $value)
+    {
+        if (isset($this->config[$name])) {
+            $this->config[$name] = $value;
+        }
+    }
+
+    /**
+     * 检查配置
+     *
+     * @access public
+     *
+     * @param  string $name 配置名称
+     *
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return isset($this->config[$name]);
+    }
+
+    # 获取接口列表
+    public function get_api_list($version = 0)
+    {
+        $list = [];
+        if ($this->config['api_type'][$version]['type'] == 1) {
+            $file = $this->listDirFiles($this->config['api_type'][$version]['app'] . '/api');
+        } else {
+            $file = $this->listDirFiles($this->config['api_type'][$version]['app'] . '/controller');
+        }
+        foreach ($file as $k => $class) {
+            $class = "app\\" . $class;
+            if (class_exists($class)) {
+
+                $reflection = new \ReflectionClass($class);
+                $doc_str = $reflection->getDocComment();
+                $doc = new Parser();
+                # 解析类
+                $class_doc = $doc->parse_class($doc_str);
+                if (!empty($class_doc)) {
+                    $list[$k] = array_merge($this->classkey, $class_doc);
+                    $list[$k]['class'] = $class;
+                    $method = $reflection->getMethods(\ReflectionMethod::IS_PUBLIC);
+                    # 过滤不需要解析的方法以及非当前类的方法(父级方法)
+                    $filter_method = array_merge(['__construct'], $this->config['filter_method']);
+                    foreach ($method as $key => $action) {
+                        if (!in_array($action->name, $filter_method) && $action->class === $class) {
+                            $res = $doc->parse_action($action);
+                            if ($res) {
+                                $list[$k]['action'][$key] = array_merge($this->actionkey, $doc->parse_action($action));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return $list;
+    }
+
+    /**
+     * 获取接口详情
+     * @param string $class
+     * @param string $action
+     *
+     * @return array|bool
+     */
+    public function get_api_detail($class = '', $action = '')
+    {
+        $method = (new \ReflectionClass($class))->getMethod($action);
+        $data = (new Parser())->parse_action($method);
+        return array_merge($this->actionkey, $data);
+    }
+
+    /**
+     * 获取文件夹内的所有文件
+     * @param string $class
+     * @param string $action
+     *
+     * @return array|bool
+     */
+    protected function listDirFiles($app, $isapp = true)
+    {
+        $arr = [];
+        $base = base_path();
+        if ($isapp) {
+            $dir = $base . $app;
+        } else {
+            $dir = $app;
+        }
+
+        if (is_dir($dir)) { //如果是目录，则进行下一步操作
+            $d = opendir($dir); //打开目录
+            if ($d) { //目录打开正常
+                while (($file = readdir($d)) !== false) { //循环读出目录下的文件，直到读不到为止
+                    if ($file != '.' && $file != '..') { //排除一个点和两个点
+                        if (is_dir($dir . '/' . $file)) { //如果当前是目录
+                            $arr = array_merge($arr, self::listDirFiles($dir . '/' . $file, false)); //进一步获取该目录里的文件
+                        } else {
+                            if (pathinfo($dir . '/' . $file)['extension'] == 'php') {
+                                $arr[] = str_replace([$base, '/', '.php'], ['', '\\', ''], $dir . '/' . $file); //进一步获取该目录里的文件
+                            }
+                        }
+                    }
+                }
+            }
+            closedir($d); //关闭句柄
+        }
+        asort($arr);
+        return $arr;
+    }
+}
